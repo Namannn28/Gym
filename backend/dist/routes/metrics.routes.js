c7fd177f89f55edb7e6e27b4620d6315c3ed370b
Naman Gupta <namang380@gmail.com>
2026-07-16 19:10:06 +0530

    build: add compiled artifact for metrics.routes.js

---
+++ backend/dist/routes/metrics.routes.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const metrics_controller_1 = require("../controllers/metrics.controller");
+const auth_middleware_1 = require("../middleware/auth.middleware");
+const router = (0, express_1.Router)();
+router.use(auth_middleware_1.authenticateToken);
+router.post('/cardio', metrics_controller_1.logCardio);
+router.get('/cardio', metrics_controller_1.getCardioLogs);
+router.post('/water', metrics_controller_1.logWater);
+router.get('/water', metrics_controller_1.getWaterLogs);
+router.post('/weight', metrics_controller_1.logWeight);
+router.get('/weight', metrics_controller_1.getWeightLogs);
+exports.default = router;
