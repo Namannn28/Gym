2be4cc65a1350c564b2e06a4f78c0ea1ee04e3a3
Naman Gupta <namang380@gmail.com>
2026-07-16 21:23:25 +0530

    build: add compiled artifact for workout.routes.js

---
+++ backend/dist/routes/workout.routes.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const workout_controller_1 = require("../controllers/workout.controller");
+const auth_middleware_1 = require("../middleware/auth.middleware");
+const router = (0, express_1.Router)();
+router.use(auth_middleware_1.authenticateToken);
+router.post('/', workout_controller_1.logWorkout);
+router.get('/', workout_controller_1.getWorkouts);
+exports.default = router;
